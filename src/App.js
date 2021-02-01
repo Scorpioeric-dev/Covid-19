527da58a978207e10df216d7ae1612f1f5412b0f
Scorpioeric-dev <ericscorpio1@gmail.com>
2021-01-31 19:12:09 -0800

    fetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import {
+  MenuItem,
+  FormControl,
+  Select,
+  Card,
+  CardContent,
+} from "@material-ui/core";
+import { InfoBox } from "./Components/InfoBox";
+import { Map } from "./Components/Map";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+  const [countryInfo, setCountryInfo] = useState({});
+
+  //useEffect() runs a piece of code  based on a given condition
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+
+    const url =
+      countryCode === "worldwide"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCountry(countryCode);
+        setCountryInfo(data);
+      });
+  };
+
+  console.log('CountryInfo', countryInfo)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/*AppLeft*/}
+      <div className="app_left">
+        <div className="app__header">
+          <h1>COVID-19 TRACKER</h1>
+          <FormControl className="app_dropdown">
+            <Select
+              variant="outlined"
+              value={country}
+              onChange={onCountryChange}
+            >
+              {/* Loop through all the countries and show a dropdown list of the options */}
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="app_stats">
+          <InfoBox title="Coronavirus Critical Cases" cases={countryInfo.critical}  />
+
+          <InfoBox title="Recovered" cases={countryInfo.recovered} />
+
+          <InfoBox title="Deaths" cases={countryInfo.deaths}  />
+        </div>
+        <Map />
+      </div>
+      <Card className="app_right">
+        <CardContent>
+          <h3>Live Cases by Country</h3>
+          <h3>WorldWide New Cases </h3>
+        </CardContent>
+        {/*Table*/}
+        {/*Graph */}
+      </Card>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Components/Map.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Map = () => {
+    return (
+        <div className='map'>
+            <h1>I am a MAP</h1>
+        </div>
+    )
+}
